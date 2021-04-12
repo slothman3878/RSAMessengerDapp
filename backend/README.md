d2e1afde179d724a03e65f707fa88091da6691fb
slothman3878 <sloth.man3878@gmail.com>
2021-04-12 17:56:16 -0400

    initial commit

---
+++ backend/README.md
@@ -0,0 +1,29 @@
+# RSA Messenger Dapp
+### Messenger dapp on Ethereum with assymetric encryption
+
+## Introduction
+  The idea is to create an online messenger service without relying on centralized servers for successful transmission and privacy of messages. Messages can be transmitted between different frontend platforms as long as they tap into the same messenger contract.<br><br>
+  This dapp works as such:
+1. When user <b>A</b> registers with the dapp, they generate a public and private key pair.
+2. The public key is stored on the ethereum blockchain. The private key is kept by <b>A</b> for safe keeping.
+3. If a user <b>B</b> wants to message <b>A</b>, they request the public key from the blockchain, and uses it to encrypt their message.
+4. The encrypted messages is stored on a decentralized storage system (in this project, IPFS is used). The CID of the message is stored on the blockchain.
+5. <b>A</b> requests the message CID from the blockchain, gets the encrypted message, and decrypts it with their private key.
+   
+  This is just standard assymetric encryption, only decentralized, hence better!
+  <br><br>
+  RSA encryption is only worth a damn if the keys are at least 2048 bits long. While one can store those keys on the blockchain, that's 256 bytes (plus bytes needed to store the public exponent) worth of blockchain memory being altered, resulting in a high gas price.
+  <br><br>
+  Here, we have an alternative: upload the public key to IPFS and store the CID on the blockchain instead, which should result in significanlty a lower gas price (theoretically).
+
+## Important!!!
+  The frontend is merely for demonstration. It should not be used seriously.
+
+## TODO list
+#### Ordered by Importance
+* Create frontend using Django, web3.py, and eth-account (currently leaning towards creating an ethereum address with a signup).
+* Pin messages and public-key using Pinata.
+* Deploy to test net.
+* Add function so that third parties can send and recieve messages on user's behalf.
+* (Maybe) Implement a version where the public keys are stored on the blockchain and compare gas prices.
+* Entrusting private servers for safe keeping of private keys is far from ideal. Need to think of alternative methods for private key storage.
---
+++ backend/contracts/Messenger.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Messenger{
+  mapping(address => string) _keys; // Contains URI to keys for each address
+  mapping(address => bool) _exists;
+
+  //Having to store the from and to addresses for each message may be needlessly costly.
+  //Information about who sent and recieved the message can be appended to the message itself.
+  struct Message{
+    //Declare if Message is type public or private? Private messsages require decryption, whereas public ones don't.
+    address from;
+    address to;
+    string message;
+  }
+
+  uint256 private _messageIndex;
+
+  mapping(uint256 => Message) private _messages;
+
+  mapping(address => uint256) private _recievedBalance; //total recieved for each address
+  mapping(address => uint256) private _sentBalance;     //total sent for each address
+  
+  mapping(address => mapping(uint256 => uint256)) private _recieved;  //recieved messages
+  mapping(address => mapping(uint256 => uint256)) private _sent;
+
+  event SetPublicKey(address user, string key);
+  event MessageSent(address from, address to);
+
+  function setPublicKey(address user, string memory keyURI) public virtual {
+    require(user == msg.sender, "Messenger: User not the transaction sender");
+    if(!_exists[user]) _exists[user] = true;
+    _keys[user]=keyURI;
+    emit SetPublicKey(user, keyURI);
+  }
+
+  function sendMessage(address from, address to, string memory URI) public virtual {
+    require(from == msg.sender, "Messenger: Message not being sent by transaction sender");
+    require(_exists[to], "Messenger: Trying to send message to nonexistent user");
+    Message memory message = Message(from, to, URI);
+
+    _messageIndex++;
+    _messages[_messageIndex]=message;
+    
+    _recieved[to][_recievedBalance[to]]=_messageIndex;
+    _recievedBalance[to]++;
+
+    _sent[from][_sentBalance[from]]=_messageIndex;
+    _sentBalance[from]++;
+
+    emit MessageSent(from, to);
+  }
+
+  function getPublicKey(address to) public virtual view returns (string memory) {
+    require(_exists[to], "Messenger: Requesting public key of nonexistent user");
+    return _keys[to];
+  }
+
+  function getMessage(uint256 id) public virtual view returns (Message memory) {
+    require(_messages[id].from!=address(0),"Messenger: Requesting nonexistent message");
+    return _messages[id];
+  }
+
+  function getRecievedMessageByIndex(uint256 id) public virtual view returns (uint256) {
+    address user = msg.sender;
+    require(id < getRecievedBalance(user), "Messenger: Index out of bounds");
+    return _recieved[user][id]; 
+  }
+
+  function getSentMessageByIndex(uint256 id) public virtual view returns (uint256) {
+    address user = msg.sender;
+    require(id < getSentBalance(user), "Messenger: Index out of bounds");
+    return _sent[user][id];
+  }
+
+  function getRecievedBalance(address user) public virtual view returns (uint256) {
+    return _recievedBalance[user];
+  }
+
+  function getSentBalance(address user) public virtual view returns (uint256) {
+    return _sentBalance[user];
+  }
+}

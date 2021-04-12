3da4cb2497c0ffc8587024f4fb0e0841a4ccd0cd
slothman3878 <sloth.man3878@gmail.com>
2021-04-12 18:08:10 -0400

    secret_key reset

--- RSAMessengerDapp/RSAMessengerDapp/settings.py
+++ RSAMessengerDapp/RSAMessengerDapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-md#b8*mikl!!dw$cwp@rvpuqz%q(yb6=!s&!ib3m&w!*o8ut*&'
+SECRET_KEY = '11111111111111111111111111111111111111111111111111111111111111'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
